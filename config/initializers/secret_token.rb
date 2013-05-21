d99eb848595a9f8149ea736234e8b006d91a7de7
yoshiori <yoshiori@gmail.com>
2013-05-21 23:11:53 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teams::Application.config.secret_token = 'd6911efddc1392a5882489a912e7686c0048455c130a0cfe860587dd44026f381aae31adc66080625cb58fbaabc1442f528241450c5cac23ad6076fecefc606e'
